cc2cd2540a19c2569413729817824a88a18bbd1a
YeJieChen <1940879828@qq.com>
2024-09-19 17:33:50 +0800

    feat: 尝试wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
 import './App.css'
 import {Outlet} from "react-router-dom";
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { WagmiProvider } from 'wagmi'
+import { arbitrum, mainnet } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// 0. Setup queryClient
+const queryClient = new QueryClient()
+
+// 1. Your Reown Cloud project ID
+const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'myWeb3LearnProject',
+  description: 'AppKit Example',
+  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum] as const
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+})
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: false // Optional - false as default
+})
 
 function App() {
-
   return (
-    <div>
-      <Outlet />
-    </div>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,44 @@
+import {useState} from "react";
+import {WagmiConfig} from "wagmi";
+
 function Home() {
+  const [isNetworkSwitchHighlighted, setIsNetworkSwitchHighlighted] =
+    useState(false);
+  const [isConnectHighlighted, setIsConnectHighlighted] = useState(false);
+
+  const closeAll = () => {
+    setIsNetworkSwitchHighlighted(false);
+    setIsConnectHighlighted(false);
+  };
+
   return (
-    <div>this is home</div>
-  );
+    <>
+      <header>
+        <div
+          style={{
+            opacity:
+              isConnectHighlighted || isNetworkSwitchHighlighted
+                ? 1
+                : 0,
+          }}
+        />
+        <div>
+          <div>
+            <div
+              onClick={closeAll}
+            >
+              <w3m-network-button />
+            </div>
+            <div
+              onClick={closeAll}
+            >
+              <w3m-button />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  )
 }
 
 export default Home;
